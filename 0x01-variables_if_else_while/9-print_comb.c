f1688fb26ecdbe7e7fc5dda6ef3c469f3d6933d8
root <4908@holbertonschool.com>
2022-05-23 08:52:33 -0700

    step 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - stuff goes here
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
