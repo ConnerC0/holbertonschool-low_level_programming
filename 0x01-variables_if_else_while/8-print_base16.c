4b14058a4646fa376b93297ca6d561a4467e3abd
root <4908@holbertonschool.com>
2022-05-23 08:38:29 -0700

    step 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - stuff goes here
+ *
+ * Return: Always 0.
+ */
+int main(void)	
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	for (i= 10; i < 16; i++)
+	{
+	if ((i > 9) && (i < 16))
+	{
+		for (i = 'a'; i <= 'f'; i++)
+			putchar(i);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
