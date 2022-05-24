1a147b292a3b0ffce281aa56e9d8228a1f0bc1cf
root <4908@holbertonschool.com>
2022-05-24 16:56:28 -0700

    step 6 and 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - stuff
+ * @i: provided integer
+ *
+ * Return: i
+ */
+int print_last_digit(int i)
+{
+	i = i % 10;
+	if (i > 0)
+	{
+	_putchar(i + '0');
+	}
+	else if (i <= 0)
+	{
+		i *= -1;
+		_putchar(i + '0');
+		return (i);
+	}
+}
