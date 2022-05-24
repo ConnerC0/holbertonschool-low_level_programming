10d7e868dfe852fd55acfe6fe89d543f950df7b0
root <4908@holbertonschool.com>
2022-05-24 06:10:18 -0700

    step 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - stuff
+ * 
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+	char c;
+	char s[] = "_putchar\n";
+	while (n <= 9)
+	{
+		c = s[n];
+		_putchar(c);
+		n++;
+	}
+	return (0);
+}
