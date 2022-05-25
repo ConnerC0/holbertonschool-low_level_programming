ab13e6e22d2e2cc796468e9255b33a58fd98d3c9
root <4908@holbertonschool.com>
2022-05-24 19:46:06 -0700

    betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,13 @@
 
 /**
  * add - stuff
- * @v1 - variable
- * @v2 - variable
+ * @v1: variable
+ * @v2: variable
  *
  * Return: v1 + v2
  */
 
 int add(int v1, int v2)
 {
-	return(v1 + v2);
+	return (v1 + v2);
 }
