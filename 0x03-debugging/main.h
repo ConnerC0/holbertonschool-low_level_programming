88375ea35473851f7db482b5f89cac2601af675f
root <4908@holbertonschool.com>
2022-05-27 00:51:24 -0700

    debugging complete

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days();
+int convert_day();
+
+#endif /* MAIN_H */
