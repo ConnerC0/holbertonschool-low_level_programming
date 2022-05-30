60236579538c8fd43d96fa98e4fc58e1cfbb2f11
root <4908@holbertonschool.com>
2022-05-30 11:37:57 -0700

    step 6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints 1-14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int n;
+	int i;
+	char c;
+	char s[] = "01234567891011121314\n";
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 15; i++)
+		{
+			c = s[i];
+			_putchar(c);
+		}
+	}
+}
