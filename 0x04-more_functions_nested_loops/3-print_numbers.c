370050d1644392e341d286742ea5c1058dca313e
root <4908@holbertonschool.com>
2022-05-30 11:19:36 -0700

    step 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints 1-9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char s[] = "0123456789\n";
+	char c;
+
+	for (i = 0; i > 9; i++)
+	{
+		c = s[i];
+		_putchar(c);
+	}
+}
