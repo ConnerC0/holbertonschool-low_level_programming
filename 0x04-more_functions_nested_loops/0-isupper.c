7e247d5807777a435b9d8ba98b2c38bd906cb288
root <4908@holbertonschool.com>
2022-05-30 10:42:24 -0700

    step 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks if upper case
+ * @c: provided integer
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+}
