6f39f6c85d168d749d696716970065270a729761
root <4908@holbertonschool.com>
2022-06-02 18:40:19 -0700

    done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints elements into array
+ * @a: provided variable
+ * @n: provided integer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int b = 0;
+
+	while (b < n)
+	{
+		printf("%d", a[b]);
+		if (b < (n - 1))
+			printf(", ");
+		b++;
+	}
+	printf('\n');
+}
