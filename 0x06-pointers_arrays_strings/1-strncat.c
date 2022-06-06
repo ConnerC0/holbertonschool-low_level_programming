5c68bf1d155080c35acfa6b00478bbf1124668e5
root <4908@holbertonschool.com>
2022-06-06 11:08:41 -0700

    steps 0-3;

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: provided variable
+ * @src: provided variable
+ * @n: provided integer
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int d = strlen(dest);
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[d + a] = src[a];
+	dest[d + a] = '\0';
+	return (dest);
+}	
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: provided variable
+ * @s2: provided variable
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = strlen(s1);
+	int b = strlen(s2);
+
+	if (a > b)
+		return (15);
+	else if (b > a)
+		return (-15);
+	else
+		return (0);
+}
+
