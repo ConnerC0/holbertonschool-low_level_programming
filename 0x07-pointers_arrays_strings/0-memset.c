ba0472d2d043b0fd0d56b3653e7ce632fd531949
root <4908@holbertonschool.com>
2022-06-07 12:29:52 -0700

    fix?

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,17 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	if (s != NULL && n > 0)
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
 	{
-		char *ps = s;
-		int i = 0;
-
-		for (i = 0; i < n; i++)
-		{
-			unsigned char temp = (unsigned char) b;
-
-			ps[i] = temp;
-		}
+		s[i] = b;
 	}
 	return (s);
 }
