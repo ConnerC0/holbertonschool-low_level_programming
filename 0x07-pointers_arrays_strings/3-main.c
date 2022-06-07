70b33515749f0b91aa02b86ab50dc21ed07d91b5
root <4908@holbertonschool.com>
2022-06-07 14:13:21 -0700

    step 3

---
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int main()
+{
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
+
+	n = _strspn(s, f);
+	printf("%u\n", n);
+	return (0);
+}
