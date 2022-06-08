1cab8ab18f575d4e328b6c131fdc634159dc8e77
root <4908@holbertonschool.com>
2022-06-07 18:48:16 -0700

    step 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sum of diag in arrays
+ * @a: provided variable
+ * @size: provided variable
+ * Return: void
+ */
+
+void print_diagsums( int *a, int size)
+{
+	int i;
+	int b = 0;
+	int c = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i % (size + 1) == 0)
+			b += a[i];
+		if (i % (size - 1) == 0 && (i > 0 && (i < (size * size - 1))))
+			c += a[i];
+	}
+	printf("%d, %d\n", b, c);
+}
