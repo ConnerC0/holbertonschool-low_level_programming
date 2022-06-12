312e6d56f012527fd2f2e7bf567f108d3daad7e9
root <4908@holbertonschool.com>
2022-06-11 23:56:02 -0700

    fix?

--- 0x0A-dynamic_libraries/2-strlen.c
+++ 0x0A-dynamic_libraries/2-strlen.c
@@ -1,19 +1,5 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * _strlen - returns the length of a string
- * @s: characters in string
- * Return: length of string
- */
 int _strlen(char *s)
 {
-	int n = 0;
-
-	while (*(s + n))
-	{
-		n++;
-	}
-
-	return (n);
+	s = 0;
+	return (0);
 }
