5a66f1d68af5738129fbae53f5e0d45c2fa32852
root <4908@holbertonschool.com>
2022-06-21 11:14:54 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= j)
 		l = i + j;
 	else
-		l = i + n
+		l = i + n;
 	s = malloc(sizeof(char) * (l + 1));
 	if (s == NULL)
 		return (NULL);
