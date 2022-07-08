59199342fca3b803e8e9d680b46fd7cb926faeac
root <4908@holbertonschool.com>
2022-07-08 06:30:05 -0700

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,11 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
-	const list_t *temp;
 
-	temp = h;
 	while (temp)
 	{
-		printf("[%u] %s\n", temp->len, temp->str);
-		temp = temp->next;
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		i++;
 	}
 	return (i);
