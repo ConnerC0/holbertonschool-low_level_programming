1f701b6744aa655952a21eaa057917eb7c06f4eb
root <4908@holbertonschool.com>
2022-07-18 16:14:10 -0700

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	// fd = creat(filename, mode);
-	// creat() is the same as open(O_CREAT | O_WRONLY | O_TRUNC)
+	/* fd = creat(filename, mode); */
+	/* creat() is the same as open(O_CREAT | O_WRONLY | O_TRUNC) */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd == -1)
 		return (-1);
