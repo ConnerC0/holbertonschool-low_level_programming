23d3dd8c0b7105ec0b20925c2f5d9eae34ff7d59
root <4908@holbertonschool.com>
2022-07-18 16:04:22 -0700

    fix?

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	mode_t mode = 0600;
 
 	if (filename == NULL)
 		return (-1);
-	fd = creat(filename, 0600);
+	fd = creat(filename, mode);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
