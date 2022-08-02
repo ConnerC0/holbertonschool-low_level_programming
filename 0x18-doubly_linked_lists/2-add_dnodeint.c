604f3a4d0e7800e5259515d13c588cd63ec2080b
ConnerC0 <104493204+ConnerC0@users.noreply.github.com>
2022-08-02 11:45:52 -0500

    Update 2-add_dnodeint.c
    Redo

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -9,19 +9,6 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	n = new->n;
-	new->prev = NULL;
-	new->next = *head;
-	if ((*head) != NULL)
-		(*head)->prev = new;
-	*head = new;
-
-	return (new);
 }
 
