a3970f5c0cbd4a39915f1ba11a79bcc90b29879a
root <4908@holbertonschool.com>
2022-08-02 17:07:08 -0700

    fixes

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	n = new->n;
+	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 	if ((*head) != NULL)
