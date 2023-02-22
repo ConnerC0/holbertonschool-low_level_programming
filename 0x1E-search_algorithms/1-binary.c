3680b1e67b065dead19bcf1d1e661ede1ef58775
ConnerC0 <connercurtis72@gmail.com>
2023-02-22 14:53:32 -0600

    complete

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - implement  binary search  algo
+ * @array: given array to search in
+ * @size: size of the array
+ * @value: value to search for
+ * Return: first index or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t left = 0, right = size - 1, mid;
+
+	if (array == NULL)
+		return (-1);
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+		{
+			printf("%i, ", array[i]);
+		}
+		printf("%i\n", array[right]);
+		mid = (left + right) / 2;
+		if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return (mid);
+		}
+
+	}
+	return (-1);
+}
