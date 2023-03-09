80dee034b26b8aaf50d39e7b018a873faa56055c
ConnerC0 <connercurtis72@gmail.com>
2023-03-08 20:28:33 -0600

    complete

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,7 @@
 /** Prototype */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int most_advanced_binary(int *array, int value, size_t low, size_t high);
+void print_it(int *arr, int begin, int end);
 #endif
